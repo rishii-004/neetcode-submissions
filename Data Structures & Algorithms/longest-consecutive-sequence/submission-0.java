0ea81156d9ec6905b52e93c0955b2903e3f00010
rishii-004 <work.rishikeshkatta@gmail.com>
2026-09-23 15:28:56 +0530

    Update: longest-consecutive-sequence - submission-0

--- Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -1,31 +1,37 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
 
-        // Store all numbers
         for (int num : nums) {
             set.add(num);
         }
 
-        int longest = 0;
+        int maxCount = 0;
 
-        for (int num : nums) {
+        for (int num : set) {
 
-            // Start only if this is the first number of a sequence
+            // Start only if num is the beginning of a sequence
             if (!set.contains(num - 1)) {
 
                 int current = num;
-                int length = 1;
+                int count = 1;
 
                 while (set.contains(current + 1)) {
                     current++;
-                    length++;
+                    count++;
                 }
 
-                longest = Math.max(longest, length);
+                maxCount = Math.max(maxCount, count);
             }
         }
 
-        return longest;
+        return maxCount;
     }
 }
+
+
+// add all the elements into a hashset, the values in the set will lie between min and max 
+// run a loop from min to max to find the sequence, if found then update the count and maxcount 
+
+// to reduce time we will have to traverse the elements in the set itself, for each element we will see if there is num-1 present if yes then go till the smallest 
+// start searching if num + 1 present and update count and maxcount 
