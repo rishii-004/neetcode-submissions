75f8197bb80a8c4f2aff62281fd5cdf4c02cd229
rishii-004 <work.rishikeshkatta@gmail.com>
2026-09-26 15:04:57 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() > t.length()) return false;
+        if(s.length() == 0) return true;
+        int ind = 0;
+        for(int i =0; i < t.length(); i++){
+            if(t.charAt(i) == s.charAt(ind)) ind++;
+            if(ind == s.length()) return true;
+        }
+        return false;
+    }
+}
